ee2ae8cbbc887f602b69637c181e9d0acb88c061
Eduard Galikhanov <godlik04@mail.ru>
2024-05-14 16:30:10 +0300

    feat: init app

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1>About</h1>
+    <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, quidem.</div>
+</template>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1>Home page</h1>
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem similique nisi aliquam adipisci quo debitis accusantium accusamus autem omnis? Ut quod consectetur, asperiores et libero ducimus minima doloremque possimus aspernatur.</div>
+</template>
+
+<style>
+
+</style>
